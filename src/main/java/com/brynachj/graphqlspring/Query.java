bc626729736a85eae05ee8ee19796acd9905b817
Brynach Jones <brynach.jones@gmail.com>
2019-04-24 21:54:56 +0100

    Added ability to query for Song in Query

--- src/main/java/com/brynachj/graphqlspring/Query.java
+++ src/main/java/com/brynachj/graphqlspring/Query.java
@@ -4,15 +4,20 @@ import com.coxautodev.graphql.tools.GraphQLQueryResolver;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.util.List;
-
 @Component
 public class Query implements GraphQLQueryResolver {
 
     @Autowired
     private ArtistService artistService;
 
-    public List<Artist> getArtists() {
+    @Autowired
+    private SongService songService;
+
+    public Iterable<Artist> getArtists() {
         return artistService.getAllArtists();
+    }
+
+    public Iterable<Song> getSongs() {
+        return songService.getSongs();
     }
 }
